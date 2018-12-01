c05b683c4173bac903bacf4306b9fb2ded7121e9
Lingling <lli53@u.rochester.edu>
2018-11-30 19:40:20 -0500

    add information. delet mission

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php include "inc/html_top.inc"; ?>
-<title>Home | Team Paris</title>
+<title>F.I.T | Team Paris</title>
 <?php include "inc/html_top2.inc"; ?>
 
 
@@ -10,12 +10,16 @@
 	</header>
 
 <?php include "inc/nav.inc"; ?>
-	<div>
+	
+	<main>
+
 		<h2> Introduction</h2>
 
 		<p>
-			Find the Ideal Type, is under the parent company HelloFresh, in the hope of expanding its market share and promoting healthy food plans, diets, and lifestyles in our whole society. People choose a specific diet plan online through the APP, and according to their choice, we will deliver fresh food box every week. The food box includes the recipe, exact amount of ingredients in small bags which will help people control their calorie intakes. There will be 3 diet plans—all lasting 21 days—including weight loss, keeping fit and weight gain. Concerning the customization, for each plan, there are 5 choices with different kinds of food. By sticking to our 21-day diet plan, people are able to reach their short-term weight management goals and develop healthy eating habits in the long run. In the meanwhile, our product enables people to enjoy cooking, get the freshest high-quality food and save their time. 
+			F.I.T (Find the Ideal Type), is under the parent company HelloFresh, in the hope of expanding its market share and promoting healthy food plans, diets, and lifestyles in our whole society. 
 		</p>
+		
+	</main>
 
 
 
---
+++ product.php
@@ -0,0 +1,58 @@
+<?php include "inc/html_top.inc"; ?>
+<title> Product Introduction| Team Paris</title>
+<?php include "inc/html_top2.inc"; ?>
+
+<body>
+
+	<header>
+		<h1> Product Introduction</h1>
+	</header>
+
+<?php include "inc/nav.inc"; ?>
+	<main>
+		<h2>Product</h2>
+		<p>
+			People choose a specific diet plan online through the APP, and according to their choice, we will deliver fresh food box every week. The food box includes the recipe, exact amount of ingredients in small bags which will help people control their calorie intakes. There will be 3 diet plans—all lasting 21 days—including weight loss, keeping fit and weight gain. Concerning the customization, for each plan, there are 5 choices with different kinds of food. By sticking to our 21-day diet plan, people are able to reach their short-term weight management goals and develop healthy eating habits in the long run. In the meanwhile, our product enables people to enjoy cooking, get the freshest high-quality food and save their time. 
+		</p>
+
+		<p>
+			We source ingredients directly from independent producers and deliver our top-quality products nationwide. Refrigerated boxes are packed with ice packs and insulated liners to ensure the ingredients will stay fresh for the full delivery day. Alter the size or quantity of refrigerants, or use thermal liners with varying levels of insulation, based on the season, weather conditions, box contents, and delivery destination. Work with family business, and source from as near to the fulfillment center as possible. 
+		</p>
+
+		<h2>Price</h2>
+			<ul>Veggie: 2*3--$8.99 per serving; 4*3--$6.99 per serving</ul>
+			<ul>Classic: 2*2, 2*3, 2*4--$8.99 per serving; 4*3--$7.49 per serving</ul>
+			<ul>Family: 4*2--$7.87; 4*3--$7.49</ul>
+
+
+		<h2>Mission and Vision</h2>
+			<p>
+				Our Vission: We build a worldwide healthy community with a small food box!
+			</p>
+
+			<p>
+				Our Mission: HelloFresh is the leading global provider of fresh food at home. Under the umbrella of the HelloFresh, F.I.T. aims to help each individual to reach their ideal body condition.
+			</p>
+
+		<h2>Promotion</h2>
+			<p>
+				Coordinate with APPs about keeping fit, local vegan restaurants and local gyms. Customer online forum or chat room: share photos as well as other experience of losing weight or gaining weight.
+			</p>
+	</main>
+
+	
+	
+	<footer>
+		<div>
+			This webpage, By Team Paris
+		</div>
+	</footer>
+
+
+
+
+</body>
+
+</html>
+
+
